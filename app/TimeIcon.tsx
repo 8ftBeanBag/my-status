c91feb727846b651d8922b4642cbec4e0cb53afc
Abi <abigailhendrick96@gmail.com>
2024-05-28 16:07:28 -0400

    updated to dash

--- app/TimeIcon.tsx
+++ app/TimeIcon.tsx
@@ -14,6 +14,6 @@ export default function TimeIcon({ status }: TTimeIcon) {
     }
     return <div className="flex justify-center items-center gap-1 md:gap-3 bg-white rounded-full px-2 md:px-4 h-full font-mono">
         {availabilityEmoji()}
-        <div className={`text-xs md:text-base ${status == EAvailability.Available ? "text-green-500" : "text-red-500"}`}>8AM:5PM EST</div>
+        <div className={`text-xs md:text-base ${status == EAvailability.Available ? "text-green-500" : "text-red-500"}`}>8AM-5PM EST</div>
     </div>
 }
